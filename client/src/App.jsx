75577217b75d8b7f99c369ee7efa1824f2ef0cb7
kofi arhin <kofiarhin69@gmail.com>
2025-09-14 07:33:33 +0100

    improved ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,17 @@ const App = () => {
     const getData = async () => {
       const res = await fetch(BASE_URL);
       if (res.ok) {
-        console.log("connected to server");
+        console.log("connected to server: ", BASE_URL);
       }
     };
 
     getData();
   }, []);
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="heading">FullStack Template</h1>
+    </div>
+  );
 };
 
 export default App;
